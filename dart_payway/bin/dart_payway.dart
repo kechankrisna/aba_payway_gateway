e47fe1ddcf58f51bf9d2900a24f37d9422bbbabc
kechankrisna <ke.chankrisna168@gmail.com>
2023-09-23 23:47:49 +0700

    example

--- dart_payway/bin/dart_payway.dart
+++ dart_payway/bin/dart_payway.dart
@@ -1,5 +1,36 @@
-import 'package:dart_payway/dart_payway.dart' as dart_payway;
+import 'package:dart_payway/dart_payway.dart';
 
-void main(List<String> arguments) {
-  
+void main(List<String> arguments) async {
+  Map env = {};
+  PaywayTransactionService.ensureInitialized(ABAMerchant(
+    merchantID: env['ABA_PAYWAY_MERCHANT_ID'] ?? '',
+    merchantApiName: env['ABA_PAYWAY_MERCHANT_NAME'] ?? '',
+    merchantApiKey: env['ABA_PAYWAY_API_KEY'] ?? '',
+    baseApiUrl: env['ABA_PAYWAY_API_URL'] ?? '',
+    refererDomain: "http://mylekha.app",
+  ));
+
+  /// create transaction
+  final service = PaywayTransactionService.instance!;
+  final tranID = service.uniqueTranID();
+
+  var _transaction = PaywayCreateTransaction(
+      amount: 6.00,
+      items: [
+        PaywayTransactionItem(name: "ទំនិញ 1", price: 1, quantity: 1),
+        PaywayTransactionItem(name: "ទំនិញ 2", price: 2, quantity: 1),
+        PaywayTransactionItem(name: "ទំនិញ 3", price: 3, quantity: 1),
+      ],
+      reqTime: service.uniqueReqTime(),
+      tranId: tranID,
+      email: 'support@mylekha.app',
+      firstname: 'Miss',
+      lastname: 'My Lekha',
+      phone: '010464144',
+      option: ABAPaymentOption.abapay_deeplink,
+      shipping: 0.0,
+      returnUrl: "https://stage.mylekha.app");
+
+  var createResponse =
+      await service.createTransaction(transaction: _transaction);
 }
